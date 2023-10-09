6c675c148e9b8e95b183e95a6d6854667c11c184
Jonathan Johansson <111742024+gogosimba@users.noreply.github.com>
2023-10-09 13:42:51 +0200

    trying to fix db
    sa

--- Models/Entities/NewsLetterEntity.cs
+++ Models/Entities/NewsLetterEntity.cs
@@ -4,11 +4,6 @@
     {
         public int Id { get; set; }
 
-        public Guid ContactUmbracoKey { get; set; }
-        public string Name { get; set; } = null!;
-
         public string Email { get; set; } = null!;
-
-        public string Message { get; set; } = null!;
     }
 }
